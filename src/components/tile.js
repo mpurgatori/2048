32bedef84e9ed76c22d203d8a8da336e48b4df9d
busster <jasonmbuss@gmail.com>
2017-10-17 20:02:37 -0500

    init and add game/tile code

---
+++ src/components/tile.js
@@ -0,0 +1,17 @@
+((() => {
+  const html = `
+    <div class="tile">
+      {{ tile.value }}
+    </div>
+  `
+
+  Vue.component("tile", {
+    template: html,
+    props: {
+      tile: {
+        type: Object,
+        required: true
+      }
+    },
+  })
+}))()
