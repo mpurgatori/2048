1dc43280c4c2f32ed9925bcdf22187b276c161dc
busster <jasonmbuss@gmail.com>
2017-10-27 22:54:31 -0500

    step_4_solution

--- src/components/game-menu.js
+++ src/components/game-menu.js
@@ -6,7 +6,10 @@
         <div class="scores space-right">
           <div class="score">
             <div class="score-title">SCORE</div>
-            <div class="score-value">0000</div>
+            <div class="score-value">{{ animatedScore }}</div>
+            <transition-group name="points" tag="div" class="points">
+              <div v-for="(pointIncrease, index) in pointsIncrease" :key="index">+ {{ pointIncrease }}</div>
+            </transition-group>
           </div>
           <div class="score">
             <div class="score-title">BEST</div>
@@ -14,20 +17,89 @@
           </div>
         </div>
       </div>
-
-      <!-- TODO 1: register click event to call newGame() -->
-      <!-- read: https://vuejs.org/v2/guide/events.html#Methods-in-Inline-Handlers -->
       <a class="button space-right" @click="newGame()">New Game</a>
+      <!-- TODO 4: Add game over modal and transition -->
+      <!-- read: https://vuejs.org/v2/guide/transitions.html#Transitioning-Single-Elements-Components -->
+      <!-- Use this markup for the game over modal
+      <!-- ------------------------------------------------------------------------
+            <div class="modal">
+              <h1>Game Over!</h1>
+              <a class="button button-black" @click="newGame()">Try again</a>
+            </div>
+       ------------------------------------------------------------------------- -->
+      <!-- implement the css for this at the bottom of the 'styles.css'
+           you will see a comment there -->
+      <transition name="fade">
+        <div v-if="gameOver" class="modal">
+          <h1>Game Over!</h1>
+          <a class="button button-black" @click="newGame()">Try again</a>
+        </div>
+      </transition>
     </div>
   `
 
   Vue.component("game-menu", {
     template: html,
 
+    props: {
+      gameOver: {
+        type: Boolean,
+        required: true,
+      }
+    },
+
+    data() {
+      return {
+        animatedScore: 0,
+        pointsIncrease: [],
+      }
+    },
+
+    watch: {
+      // read: https://vuejs.org/v2/guide/transitioning-state.html#Animating-State-with-Watchers
+      score(newValue, oldValue) {
+        const self = this
+
+        if (newValue > 0) {
+          // must clone deep because when mutating (NOT replacing) an Array, 
+          // the old value will be the same as new value because they reference the same Array
+          let oldPoints = _.cloneDeep(self.pointsIncrease)
+          oldPoints.push(newValue - oldValue)
+          self.pointsIncrease = oldPoints
+        }
+
+        function animate () {
+          if (TWEEN.update()) {
+            requestAnimationFrame(animate)
+          }
+        }
+        new TWEEN.Tween({ tweeningNumber: oldValue })
+          .easing(TWEEN.Easing.Quadratic.Out)
+          .to({ tweeningNumber: newValue }, 500)
+          .onUpdate(function () {
+            self.animatedScore = this.tweeningNumber.toFixed(0)
+          })
+          .start()
+        animate()
+      },
+
+      pointsIncrease(newPoints, oldPoints) {
+        if (newPoints.length > oldPoints.length) {
+          setTimeout(() => {
+            this.pointsIncrease.pop()
+          }, 200)
+        }
+      }
+    },
+
+    computed: {
+      score() {
+        return this.$store.state.score
+      }
+    },
+
     methods: {
       newGame() {
-        // TODO 2: emit custom event
-        // https://vuejs.org/v2/guide/components.html#Using-v-on-with-Custom-Event
         this.$emit("new-game")
       }
     }
--- src/components/tile.js
+++ src/components/tile.js
@@ -1,7 +1,11 @@
 ((() => {
   const html = `
-    <div class="tile">
-      {{ tile.value }}
+    <!-- TODO 1: Implement class binding for 'tile-empty' class from emptyTile computed property value
+                 Implement style binding for css style 'backgroundColor' from backgroundColor computed property value -->
+    <!-- read: https://v1.vuejs.org/guide/class-and-style.html#Binding-HTML-Classes
+               https://v1.vuejs.org/guide/class-and-style.html#Binding-Inline-Styles -->
+    <div class="tile" v-bind:style="{backgroundColor: backgroundColor}" v-bind:class="{'tile-empty': emptyTile}">
+      {{ displayingValue }}
     </div>
   `
 
@@ -13,5 +17,73 @@
         required: true
       },
     },
+
+    data() {
+      return {
+        colors: [
+          "",
+          "#90CCC8",
+          "#90C2CE",
+          "#8FB2D1",
+          "#8FA1D3",
+          "#8F8ED6",
+          "#A28DD8",
+          "#B78CDB",
+          "#CE8CDD",
+          "#E08BD9",
+          "#E28AC3",
+          "#E589AC",
+          "#E78792",
+          "#EA9686",
+          "#ECB185",
+          "#EFCE84",
+          "#F1ED82",
+        ]
+      }
+    },
+
+    watch: {
+      // TODO 3: add watcher on value to animate the scale of the tile when its value increases
+      // hint: you can use this method for the tile animation
+      //    setTimeout(() => {
+      //      Velocity(this.$el, {scale: 1.2}, {duration: 50, complete: () => {
+      //        Velocity(this.$el, {scale: 1}, {duration: 50})
+      //      }})
+      //     }, 50)
+      // read: https://vuejs.org/v2/guide/computed.html#Watchers
+      value(newVal, oldVal) {
+        if (newVal > oldVal) {
+          setTimeout(() => {
+            Velocity(this.$el, {scale: 1.2}, {duration: 50, complete: () => {
+              Velocity(this.$el, {scale: 1}, {duration: 50})
+            }})
+          }, 50)
+        }
+      }
+
+    },
+
+    computed: {
+      value() {
+        return this.tile.value
+      },
+
+      displayingValue() {
+        if (this.value > 0) {
+          return this.value
+        }
+
+        return null
+      },
+
+      emptyTile() {
+        return this.displayingValue === null
+      },
+
+      backgroundColor() {
+        return this.colors[this.value.toString(2).length - 1]
+      }
+
+    }
   })
 }))()
